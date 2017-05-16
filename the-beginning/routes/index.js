756809fd51edce7f04da13bbb0ff31627ab31814
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-05-16 15:57:00 -0400

    added the route

--- the-beginning/routes/index.js
+++ the-beginning/routes/index.js
@@ -3,7 +3,18 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const george = { name: 'George', age: '28', cool: true };
+  //res.send('IT Works');
+  //res.json(george);
+  //res.send(req.query.age);
+  //res.json(req.query);
+  res.render();
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const name = req.params.name;
+  const reversedName = [...name].reverse().join('');
+  res.send(reversedName);
 });
 
 module.exports = router;
